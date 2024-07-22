f5dc7836d905da0c752051752d0abd62f47066cf
Lukas <lukas.pollmann@tum.de>
2024-07-22 09:37:38 +0000

    chore: Update dependencies and add setup instructions in README

---
+++ demo-engelv-main/app.py
@@ -0,0 +1,104 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import streamlit as st
+import googlemaps
+from helper import get_places, generate_location_desc, extract_details_from_groundplan, get_coordinates_for_address
+from secrets_env import GMAPS_API_KEY
+from vertexai.preview.generative_models import Part
+from helper import generate
+
+gmaps = googlemaps.Client(GMAPS_API_KEY)
+
+st.sidebar.image("media/logo-vonovia.png")
+st.sidebar.subheader("Automatisierte Generierung von Immobilien-Exposés")
+st.sidebar.text_input("Adresse der Immobilie", key="address")
+
+lat, lng = None, None
+
+column_config = {
+    "name": "Name",
+    "lat": "Breitengrad",
+    "lng": "Längengrad"
+    }
+
+if st.session_state.address:
+    st.header("1) Details aus Google Maps", divider="rainbow")
+    st.subheader(":station: Haltestellen")
+    lat, lng = get_coordinates_for_address(st.session_state.address)
+
+    print(f"Received lat, lng: {lat}, {lng}")
+
+    subway_stations = get_places("subway_station", lat, lng)
+    print("subway stations retrieved")
+    st.dataframe(subway_stations, height=200, use_container_width=True, hide_index=True, column_config=column_config)
+
+    st.subheader(":bread: Bäckereien")
+    bakeries = get_places("bakery", lat, lng, radius=700)
+    print("bakeries retrieved")
+    st.dataframe(bakeries, height=200, use_container_width=True, hide_index=True, column_config=column_config)
+
+    st.subheader(":office: Stadt-Zentrum")
+    city_hall = get_places("city_hall", lat, lng, radius=1500)
+    st.dataframe(city_hall, use_container_width=True, hide_index=True, column_config=column_config)
+    
+    st.header("2) Lagebeschreibung für das Exposé via Gemini", divider="rainbow")
+
+    prompt = f"""
+    Schreibe einen Text für die Lage eines Immobilieninserats für eine Wohnung
+    Du bist ein Immobilien-Experte
+    Schreibe positiv und begeistere Kunden
+    Schreibe eine griffige Überschrift mit Bezug auf die Lage der Immobilie
+    Liste 2 bis 3 Beispiele zu öffentlichen Verkehrsmitteln und Bäckereien
+    Nutze valides Markdown
+
+    Öffentliche Verkehrsmittel:
+    {subway_stations}
+
+    Bäckereien:
+    {bakeries}
+
+    Entfernung zum Stadt-Zentrum:
+    {city_hall}
+    """
+
+    prompt_override = st.text_area("Prompt für das LLM", value=prompt, height=500)
+
+    if prompt_override:
+        prompt = prompt_override
+
+    if st.button("Generiere den Text", type="primary", key="button_generate"):
+        st.write(generate_location_desc(prompt))
+        st.session_state.clicked = False
+    
+    st.header("2) Extrahieren der Basis-Daten aus dem Grundriss", divider="rainbow")
+
+    groundplan = st.file_uploader("Grundriss hochladen")
+    if not groundplan:
+        button_disabled = True
+    
+    if groundplan:
+        st.image(groundplan.getvalue())
+        button_disabled = False
+    
+    if st.button("Extrahiere Basis-Daten", type="primary", key="button_extract_from_groundplan", disabled=button_disabled):
+        st.dataframe(extract_details_from_groundplan(groundplan.getvalue()), use_container_width=True, column_config={
+            "value": "Wert"
+        })
+        st.session_state.clicked = False
+
+
+
+
---
+++ demo-engelv-main/helper.py
@@ -0,0 +1,137 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import vertexai
+from vertexai.preview.generative_models import GenerativeModel, Part
+import vertexai.preview.generative_models as generative_models
+import os
+import streamlit as st
+import googlemaps
+import re
+from secrets_env import GMAPS_API_KEY
+
+gmaps = googlemaps.Client(GMAPS_API_KEY)
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "key-app-engelv.json"
+
+def generate(prompt, vision=False):
+   vertexai.init(project="demos-engel-voelkers", location="europe-west3")
+
+   if vision:
+      model = GenerativeModel("gemini-1.0-pro-vision-001")
+   else:
+      model = GenerativeModel("gemini-1.0-pro-001")
+   
+   responses = model.generate_content(
+      prompt,
+      generation_config={
+         "max_output_tokens": 2048,
+         "temperature": 0.4,
+         "top_p": 1
+         },
+         safety_settings={
+            generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            }, 
+            stream=False)
+   
+   print("GENERATE")
+   print(responses.text)
+   
+   return responses.text
+
+
+def extract_names(content_json):
+  prompt_extract_names = f"""
+  Give me all values from the name, lat and lng. 
+  Put them in a list of dictionaries. Convert Umlauts.
+  Output only dictionary
+  
+  Input: {content_json}
+  """
+  print("EXTRACT NAMES")
+  response = generate(prompt_extract_names)
+  print(response)
+
+  response = re.findall(r"\[{1}[^\]]+\]", response)[0]
+  print("RE ----------------")
+  print(response)
+  print("--------------")
+
+  return response
+
+@st.cache_data
+def get_places(type: str, lat: str, lng: str, radius=1000):
+
+    places = gmaps.places_nearby(radius=radius, location=f"{lat},{lng}", type=type)
+    print("places received")
+    places_extracted = extract_names(places)
+    places_extracted = eval_json(places_extracted)
+    places_with_distances = eval(places_extracted)
+
+    print("names extracted")
+    print(places_extracted)
+
+    distances = gmaps.distance_matrix(origins=f"{lat},{lng}", destinations=places_with_distances, mode="walking")
+
+    for idx, distance in enumerate(distances.get("rows")[0].get("elements")):
+       places_with_distances[idx]
+       places_with_distances[idx]["Gehweg in Minuten"] = distance.get("duration").get("text")
+
+    print("GET PLACES")
+    print(places_with_distances)
+    
+    return places_with_distances
+
+def generate_location_desc(prompt: str):
+   print("GENERATE LOCATION DESC")
+   print("prompt")
+   return generate(prompt)
+
+def extract_details_from_groundplan(groundplan: bytes):
+    groundplan = Part.from_data(data=groundplan, mime_type="image/jpeg")
+
+    groundplan1 = open("media/Grundriss1.jpeg", "br").read()
+    groundplan1 = Part.from_data(data=groundplan1, mime_type="image/jpeg")
+
+    groundplan2 = open("media/Grundriss2.jpg", "br").read()
+    groundplan2 = Part.from_data(data=groundplan2, mime_type="image/jpeg")
+
+    prompt = ["""Du bist ein Experte für Immobilien
+              Die Küche, die Garage, HWR, Flur, Garderrobe und das Badezimmer zählen nicht als Zimmer
+              Loggia wird mit einem Balkon gleichgesetzt""", 
+              groundplan1, 
+              """{\"Anzahl_zimmer\": 3, \"Wohnzimmer\": 
+              \"ja\", \"Schlafzimmer\":\"ja\", \"Kinderzimmer\":\"ja\", \"Badezimmer\":\"ja\", \"Küche\":\"ja\", 
+              \"Garage\":\"nein\", \"Balkon\":\"ja\", \"Arbeitszimmer\":\"nein\"}""",
+              groundplan2, """{\"Anzahl_zimmer\": 3, 
+              \"Wohnzimmer\": \"ja\", \"Schlafzimmer\":\"ja\", \"Kinderzimmer\":\"ja\", \"Badezimmer\":\"ja\", \"Küche\":\"ja\", 
+              \"Garage\":\"ja\", \"Balkon\":\"nein\", \"Arbeitszimmer\":\"ja\"}""",
+              groundplan]
+    
+    return eval(generate(prompt, vision=True))
+
+def get_coordinates_for_address(address: str):
+    print("GET COORDINATES")
+    geocode = gmaps.geocode(st.session_state.address)[0].get("geometry").get("location")
+    lat = geocode.get("lat")
+    lng = geocode.get("lng")
+
+    return lat, lng
+
+def eval_json(data: str):
+   return re.findall(r"\[{1}[^\]]+\]", data)[0]
